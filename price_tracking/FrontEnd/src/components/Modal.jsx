3e0ab77e1a3f8928e225ec887b8e457fbb0a6e41
VirajCommits <virajmurab567@yahoo.com>
2024-05-23 11:47:45 +0530

    Done Modal class,next PriceHistory

---
+++ price_tracking/FrontEnd/src/components/Modal.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Modal.css';
+
+function Modal({ show, handleClose, children }) {
+  const showHideClassName = show ? "modal display-block" : "modal display-none";
+
+  return (
+    <div className={showHideClassName}>
+      <section className="modal-main">
+        {children}
+        <button type="button" onClick={handleClose}>
+          Close
+        </button>
+      </section>
+    </div>
+  );
+}
+
+export default Modal;
