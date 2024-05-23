0a9a7b6ec94b19cff536baf31a03f97c9cc2e8a0
VirajCommits <virajmurab567@yahoo.com>
2024-05-23 13:17:11 +0530

    Done App.js and added a bit of css

--- price_tracking/FrontEnd/src/App.js
+++ price_tracking/FrontEnd/src/App.js
@@ -1,23 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import SearchTextList from "./components/SearchTextList";
+import PriceHistoryTable from "./components/PriceHistoryTable";
+import axios from "axios";
+import TrackedProductList from "./components/TrackedProductList";
+
+const URL = "http://localhost:5000";
 
 function App() {
+  const [showPriceHistory, setShowPriceHistory] = useState(false);
+  const [priceHistory, setPriceHistory] = useState([]);
+  const [searchTexts, setSearchTexts] = useState([]);
+  const [newSearchText, setNewSearchText] = useState("");
+
+  useEffect(() => {
+    fetchUniqueSearchTexts();
+  }, []);
+
+  const fetchUniqueSearchTexts = async () => {
+    try {
+      const response = await axios.get(`${URL}/unique_search_texts`);
+      const data = response.data;
+      setSearchTexts(data);
+    } catch (error) {
+      console.error("Error fetching unique search texts:", error);
+    }
+  };
+
+  const handleSearchTextClick = async (searchText) => {
+    try {
+      const response = await axios.get(
+        `${URL}/results?search_text=${searchText}`
+      );
+
+      const data = response.data;
+      setPriceHistory(data);
+      setShowPriceHistory(true);
+    } catch (error) {
+      console.error("Error fetching price history:", error);
+    }
+  };
+
+  const handlePriceHistoryClose = () => {
+    setShowPriceHistory(false);
+    setPriceHistory([]);
+  };
+
+  const handleNewSearchTextChange = (event) => {
+    setNewSearchText(event.target.value);
+  };
+
+  const handleNewSearchTextSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      await axios.post(`${URL}/start-scraper`, {
+        search_text: newSearchText,
+        url: "https://amazon.ca",
+      });
+
+      alert("Scraper started successfully");
+      setSearchTexts([...searchTexts, newSearchText]);
+      setNewSearchText("");
+    } catch (error) {
+      alert("Error starting scraper:", error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <h1>Product Search Tool</h1>
+      <form onSubmit={handleNewSearchTextSubmit}>
+        <label>Search for a new item:</label>
+        <input
+          type="text"
+          value={newSearchText}
+          onChange={handleNewSearchTextChange}
+        />
+        <button type="submit">Start Scraper</button>
+      </form>
+      <SearchTextList
+        searchTexts={searchTexts}
+        onSearchTextClick={handleSearchTextClick}
+      />
+      <TrackedProductList />
+      {showPriceHistory && (
+        <PriceHistoryTable
+          priceHistory={priceHistory}
+          onClose={handlePriceHistoryClose}
+        />
+      )}
     </div>
   );
 }
